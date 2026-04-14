6a9b8cc0ba335ba2c34fc729d8f298d46ae6b195
ROHIT SINGH <rohitsi2252@gmail.com>
2026-04-15 02:01:13 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,21 @@
+import pkg from "pg";
+const { client } = pkg;
+
+
+const database = new client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+try{
+    await database.connect();
+    console.log("connected to the database successfully");
+}catch(error){
+    console.error("Database connection failed:", error);
+    process.exit(1);
+}
+
+export default database;
